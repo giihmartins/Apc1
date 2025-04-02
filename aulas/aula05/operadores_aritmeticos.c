793d9116598ed9bf34a21f0d8782feaf948f28e8
giovanna <giovannamartins50@gmail.com>
2025-04-02 10:53:47 -0300

    codigos das aulas 4 e 5

---
+++ aulas/aula05/operadores_aritmeticos.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    // + soma
+    // - subtracao
+    // * multiplicacao
+    // / divisao
+    // % resto da divisao
+
+    int numero1 = 10;
+    int numero2 = 20;
+    int soma = numero1 + numero2;
+    printf("A soma de %i e %i = %i\n", numero1, numero2, soma);
+
+    int subtracao = numero1 - numero2;
+    printf("A subtracao de %i e %i = %i\n", numero1, numero2, subtracao);
+
+    int multipliacao = numero1 * numero2;
+    printf("A multiplicao de %i e %i = %i\n", numero1, numero2, multipliacao);
+
+    float divisao = numero1 * 1.0f / numero2;
+    printf("A divisao de %i e %i = %.1f\n", numero1, numero2, divisao);
+
+    int resto = numero1 % numero2;
+    printf("O resto da divisao de %i e %i = %i\n", numero1, numero2, resto);
+
+    return 0;
+}
