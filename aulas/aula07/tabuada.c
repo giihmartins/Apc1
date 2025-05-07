4fce5f08695e55ef28c18d27a8ae543977d09dbd
giovanna <giovannamartins50@gmail.com>
2025-05-07 11:04:31 -0300

    Arquivos auula 07

--- aulas/aula07/tabuada.c
+++ aulas/aula07/tabuada.c
@@ -3,32 +3,46 @@
 int main() {
     int numero = 0;
 
-    printf("Entre com um numero inteiro entre 1 a 10: ");
-    scanf("%i" , &numero);
+    // printf("Entre com um numero inteiro entre 1 e 10: ");
+    // scanf("%i", &numero);
+    // while (getchar() != '\n'); //limpar buffer do teclado
 
-    // printf("A tabuada de %i eh\n" , numero);
-    // printf("1 x %i\n" , numero, 1 * numero);    
-    // printf("2 x %i\n" , numero, 2 * numero);    
-    // printf("3 x %i\n" , numero, 3 * numero);    
-    // printf("4 x %i\n" , numero, 4 * numero);    
-    // printf("5 x %i\n" , numero, 5 * numero);    
-    // printf("6 x %i\n" , numero, 6 * numero);    
-    // printf("7 x %i\n" , numero, 7 * numero);    
-    // printf("8 x %i\n" , numero, 8 * numero);    
-    // printf("9 x %i\n" , numero, 9 * numero);    
-    // printf("10 x %i\n" , numero, 10 * numero);    
+    // while (numero < 1 || numero > 10) {
+    //     printf("Numero invalido. Tente novamente\n");
+    //     printf("Entre com um numero inteiro entre 1 e 10: ");
+    //     scanf("%i", &numero);
+    //     while (getchar() != '\n'); //limpar buffer do teclado
+    // }
 
-    for(int i = 1; i <= 10; i++) {
-        printf("%i x %i = %i\n" , i, numero, i * numero);
+    do {
+        printf("Entre com um numero inteiro entre 1 e 10: ");
+        scanf("%i", &numero);
+        while (getchar() != '\n'); //limpar buffer do teclado
+    } while (numero < 1 || numero > 10);
+
+    printf("A tabuada de %i eh\n", numero);
+    // printf("1 x %i = %i\n", numero, 1 * numero);
+    // printf("2 x %i = %i\n", numero, 2 * numero);
+    // printf("3 x %i = %i\n", numero, 3 * numero);
+    // printf("4 x %i = %i\n", numero, 4 * numero);
+    // printf("5 x %i = %i\n", numero, 5 * numero);
+    // printf("6 x %i = %i\n", numero, 6 * numero);
+    // printf("7 x %i = %i\n", numero, 7 * numero);
+    // printf("8 x %i = %i\n", numero, 8 * numero);
+    // printf("9 x %i = %i\n", numero, 9 * numero);
+    // printf("10 x %i = %i\n", numero, 10 * numero);
+
+    for(int i = 1; i <= 10; i++) { // 1 a 10
+      printf("%i x %i = %i\n", i, numero, i * numero);
     }
-    
+
     for(int i=10; i >=1; i--) { // 10 a 1
-        printf("%i x %i = %i\n" , i, numero, i * numero);
+        printf("%i x %i = %i\n", i, numero, i * numero);
     }
 
-    for(int i=1; i > 0; i++) {
-        printf("Ao infinito e alem!\n");
-    }
+    // for(int i=1; i > 0; i++) {
+    //     printf("Ao infinito e alem!\n");
+    // }
 
     return 0;
 }
